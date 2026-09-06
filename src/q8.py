de6c0309ac7e3c50eb054dea045493cca71933fa
Janislim6513 <h9dyjzm6rg@privaterelay.appleid.com>
2026-09-06 19:40:25 +0800

    Update q8.py

--- src/q8.py
+++ src/q8.py
@@ -20,19 +20,25 @@ Task
 
 (a) What does the buggy version actually return for [1, 2, 3, 4, 5, 6, 8], and why?
 
-    Answer:
+    Answer: The buggy version returns 3, because it counts the odd numbers 1, 3 and 5. the condition is counting
+    odd numbers instead of even numbers.
 
 (b) Fix the bug. Write the corrected function below.
     (A one-character change is enough, but you must understand why.)
 """
 
 def count_evens(numbers):
-    # your corrected code here
+    count = 0
+        for n in numbers:
+            if n % 2 == 0:      
+                count = count + 1
+        return count
     pass
 
 
 """
 (c) In one sentence, explain in plain English what `n % 2 == 0` checks.
 
-    Answer:
+    Answer: It checks whther a number is even by checking that there is no remainder when devided by 2. the % is called modulo
+    For example, 6 % 2 gives 0 because 6 divides evenly by 2.
 """
